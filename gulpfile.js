63b9aa8e7b91277c1c79256b5918b46beef6a60a
Jason <jasonhewison@chunkysamurai.com>
2014-10-02 13:26:59 +0100

    Release v0.0.4

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,9 @@ gulp.task('tag', ['bump'], function () {
 
   return gulp.src('./')
     .pipe(git.commit(message))
-    .pipe(git.tag(v, message))
+    .pipe(git.tag(v, message, function (err) {
+      if (err) throw err;
+    }))
     .pipe(git.push('origin', 'master', '--tags'))
     .pipe(gulp.dest('./'));
 });
